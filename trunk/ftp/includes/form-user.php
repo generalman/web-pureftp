59954ab9b100156e40755fbe79852411d3c6a302
olivier.legras@mass-n.com <olivier.legras@mass-n.com@d7ab7ff4-8709-cf8f-4d4a-c30d58889757>
2011-07-25 15:30:42 +0000

    Changement de couleur du titre des formes --> Blanc

--- trunk/ftp/includes/form-user.php
+++ trunk/ftp/includes/form-user.php
@@ -11,7 +11,7 @@
 		echo ("<table class=\"banniere\" border=\"0\" cellpadding=\"0\" cellspacing=\"0\" width=\"850\">\n");
 		echo ("<tr >\n");
 		echo ("<td>\n");
-		echo ("<img hspace=\"1\" src=\"$LocationImages/sii.png\" align=\"middle\" border=\"0\"><font size=\"+1\">&nbsp;".$Translate[0]."</font>\n");
+		echo ("<img hspace=\"1\" src=\"$LocationImages/sii.png\" align=\"middle\" border=\"0\"><span class=\"titre\"<font size=\"+1\">&nbsp;".$Translate[0]."</font></span>\n");
 		echo ("</td>\n");
 		echo ("<td align=\"right\">");
 		if ($Client!="Administrator"){
